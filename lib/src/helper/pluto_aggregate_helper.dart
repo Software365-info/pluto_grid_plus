31da83c238f919e8567b76358698278f769b672e
Software365-info <info@software365.info>
2024-07-22 18:09:21 +0200

    made average and sum aggregates ignore null values

--- lib/src/helper/pluto_aggregate_helper.dart
+++ lib/src/helper/pluto_aggregate_helper.dart
@@ -1,5 +1,5 @@
 import 'package:collection/collection.dart'
-    show IterableNumberExtension, IterableExtension;
+    show IterableExtension, IterableNullableExtension, IterableNumberExtension;
 import 'package:pluto_grid_plus/pluto_grid_plus.dart';
 
 class PlutoAggregateHelper {
@@ -20,7 +20,7 @@ class PlutoAggregateHelper {
         : rows;
 
     final Iterable<num> numbers = foundItems.map(
-      (e) => e.cells[column.field]!.value,
+      (e) => e.cells[column.field]?.value,
     );
 
     return numberColumn.toNumber(numberColumn.applyFormat(numbers.sum));
@@ -42,9 +42,11 @@ class PlutoAggregateHelper {
         ? rows.where((row) => filter(row.cells[column.field]!))
         : rows;
 
-    final Iterable<num> numbers = foundItems.map(
-      (e) => e.cells[column.field]!.value,
-    );
+    final Iterable<num> numbers = foundItems
+        .map(
+          (e) => e.cells[column.field]?.value as num?,
+        )
+        .whereNotNull();
 
     return numberColumn.toNumber(numberColumn.applyFormat(numbers.average));
   }
